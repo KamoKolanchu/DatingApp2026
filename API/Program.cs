648c65eb8d9bcafeb2eb6db692f438cb07eea5aa
Kamo <2020447641@ufs4life.ac.za>
2026-03-04 14:24:03 +0200

    End of Section 4 Video 41

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 // CSIP6833 (2026)
 // K. Kolanchu (2020447641)
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(opts =>
     opts.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
